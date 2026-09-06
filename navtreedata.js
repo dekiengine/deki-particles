c4b0969d18ea1edb7167a18841eb3fa34237193b
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-06 18:20:47 +0200

    Docs for deki-particles from deki-editor 9500697

--- navtreedata.js
+++ navtreedata.js
@@ -26,8 +26,7 @@ var NAVTREE =
 [
   [ "deki-particles", "index.html", [
     [ "Components", "index.html#autotoc_md0", null ],
-    [ "Feature flags", "index.html#autotoc_md1", null ],
-    [ "Component reference", "index.html#autotoc_md2", [
+    [ "Component reference", "index.html#autotoc_md1", [
       [ "ParticleEmitterComponent", "index.html#c-particleemittercomponent", null ]
     ] ]
   ] ]
